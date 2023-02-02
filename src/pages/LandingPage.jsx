1fc968a40a7526bef7987510d9aa0d6ebf37d084
Tiku <indranilchutia0308@gmail.com>
2023-02-02 13:02:59 +0530

    updates

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,12 +3,14 @@ import { Button, Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 function LandingPage() {
-  return (
-    <Container className='d-flex justify-content-center align-items-center vh-100'>
-        <Button variant="dark" className='m-4'><Link to={'/signup'} className="text-decoration-none text-white">Sign Up</Link></Button>
-        <Button variant="dark" className='m-4'><Link to={'/login'} className="text-decoration-none text-white">Login</Link></Button>
-        
-    </Container>
+
+    document.title = 'Home';
+    return (
+        <Container className='d-flex justify-content-center align-items-center vh-100'>
+            <Button variant="dark" className='m-4'><Link to={'/signup'} className="text-decoration-none text-white">Sign Up</Link></Button>
+            <Button variant="dark" className='m-4'><Link to={'/login'} className="text-decoration-none text-white">Login</Link></Button>
+            
+        </Container>
     
   )
 }
