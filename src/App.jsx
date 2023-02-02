c9c974454248731106fee08a1c71728c5989632f
Tiku <indranilchutia0308@gmail.com>
2023-02-02 12:45:26 +0530

    Landing page with Signup/Login Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -8,6 +9,7 @@ import Signup from './pages/Signup';
 function App() {
   return (
     <Routes>
+      <Route path='/' element={<LandingPage/>}/>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/login' element={<Login/>}/>
     </Routes>
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+function LandingPage() {
+  return (
+    <Container className='d-flex justify-content-center align-items-center vh-100'>
+        <Button variant="dark" className='m-4'><Link to={'/signup'} className="text-decoration-none text-white">Sign Up</Link></Button>
+        <Button variant="dark" className='m-4'><Link to={'/login'} className="text-decoration-none text-white">Login</Link></Button>
+        
+    </Container>
+    
+  )
+}
+
+export default LandingPage
